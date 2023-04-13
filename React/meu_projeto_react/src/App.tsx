6c2460c1483af056e1f8d1aadfc34d7c9e1f18f8
jose <jose.weverton.uni@gmail.com>
2023-04-12 22:18:14 -0300

    atualizando

---
+++ React/meu_projeto_react/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import './style/style.css';
+
+function App() {
+  return (
+    <div>
+      <header className="cabecalho">
+        Cabeçalho;
+      </header>
+
+      <main className="_body">
+        Corpo <br />
+        Teste
+      </main>
+
+      <footer className="rodape">
+        Rodapé
+      </footer>
+    </div>
+  );
+} export default App;
+
+
