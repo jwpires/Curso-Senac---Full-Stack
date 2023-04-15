cfa1d2c3418573cbaf3383f0d306704b3e65cc52
jose <jose.weverton.uni@gmail.com>
2023-04-14 22:10:57 -0300

    Atualizando

--- React/meu_projeto_react/src/App.tsx
+++ React/meu_projeto_react/src/App.tsx
@@ -3,10 +3,23 @@ import './App.css';
 import './style/style.css';
 
 function App() {
+
+  let Menu1: string = "Página Principal";
+  let Menu2: string = "Nossos Produtos";
+  let Menu3: string = "Contatos";
+  let Menu4: string = "Meu GitHub";
+
   return (
     <div>
       <header className="cabecalho">
-        Cabeçalho;
+        <table className='menu'>
+          <tr>
+           <a href="http://localhost:3000/"><td>{Menu1}</td></a> 
+           <a href="http://localhost:3000/"><td>{Menu2}</td></a> 
+           <a href="http://localhost:3000/"><td>{Menu3}</td></a> 
+           <a href="https://github.com/jwpires/Curso-Senac---Full-Stack" target="_blank"><td>{Menu4}</td></a> 
+          </tr>
+        </table>
       </header>
 
       <main className="_body">
