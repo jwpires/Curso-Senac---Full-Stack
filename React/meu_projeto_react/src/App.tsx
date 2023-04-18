4d3e1e45fad9c6e366e796666aa865faa394f6eb
jose <jose.weverton.uni@gmail.com>
2023-04-17 21:53:47 -0300

    atualizando

--- React/meu_projeto_react/src/App.tsx
+++ React/meu_projeto_react/src/App.tsx
@@ -2,34 +2,61 @@ import React from 'react';
 import './App.css';
 import './style/style.css';
 
-function App() {
+const Tabela = (props: { Menu1: string ; Menu2: string ;  Menu3: string ; Menu4: string ;}) =>{
+  return(
+    <table className='menu'>
+        <tr>
+            <a href="http://localhost:3000/"><td>{props.Menu1}</td></a> 
+            <a href="http://localhost:3000/"><td>{props.Menu2}</td></a> 
+            <a href="http://localhost:3000/"><td>{props.Menu3}</td></a> 
+            <a href="https://github.com/jwpires/Curso-Senac---Full-Stack" target="_blank"><td>{props.Menu4}</td></a> 
+        </tr>
+      </table>
+  );
+}
 
-  let Menu1: string = "Página Principal";
-  let Menu2: string = "Nossos Produtos";
-  let Menu3: string = "Contatos";
-  let Menu4: string = "Meu GitHub";
+const Cabecalho = () => {
+  // let Menu1: string = "Página Principal";
+  // let Menu2: string = "Nossos Produtos";
+  // let Menu3: string = "Contatos";
+  // let Menu4: string = "Meu GitHub";
 
-  return (
-    <div>
-      <header className="cabecalho">
-        <table className='menu'>
-          <tr>
-           <a href="http://localhost:3000/"><td>{Menu1}</td></a> 
-           <a href="http://localhost:3000/"><td>{Menu2}</td></a> 
-           <a href="http://localhost:3000/"><td>{Menu3}</td></a> 
-           <a href="https://github.com/jwpires/Curso-Senac---Full-Stack" target="_blank"><td>{Menu4}</td></a> 
-          </tr>
-        </table>
-      </header>
+  return(
+    <header className="cabecalho">
+      Olá
+      <Tabela Menu1={'Página Principal'} Menu2={'Nossos Produtos'} Menu3={'Contatos'} Menu4={'Meu GitHub'}/>
+    </header>
+  );
+}
 
+const Rodape = () =>{
+  return(
+    <footer className="rodape">
+        Rodapé
+    </footer>
+  );
+}
+
+const Main = () =>{
+
+  return(
       <main className="_body">
         Corpo <br />
         Teste
       </main>
+  );
+}
 
-      <footer className="rodape">
-        Rodapé
-      </footer>
+function App() {
+
+  return (
+    <div>
+
+      <Cabecalho/>
+      <Main/>
+      <Rodape/>
+
+      
     </div>
   );
 } export default App;
